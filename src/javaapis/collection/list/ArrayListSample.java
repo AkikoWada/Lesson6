3bfd14435be5899f42b3e799ce5d41a35289c013
AkikoWada <ak.wada@gmail.com>
2023-02-04 20:57:02 +0900

    Lesson6で作成したコードのコミット

---
+++ src/javaapis/collection/list/ArrayListSample.java
@@ -0,0 +1,20 @@
+package javaapis.collection.list;
+
+import java.util.ArrayList;
+
+public class ArrayListSample {
+
+    public static void main(String[] args) {
+
+        // 行きたい国や場所をリストにします
+        ArrayList<String> al = new ArrayList<String>();
+        al.add("アイスランド");
+        al.add("スペイン");
+        al.add("ナポリ");
+        al.add("リスボン");
+        al.add("ヨセミテ国立公園");
+        
+
+    }
+
+}
